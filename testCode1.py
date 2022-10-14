5ead71a3b8337b2f2baf8f23774d3eaa5399e4a2
BlaiseComo <comoblaise@gmail.com>
2022-10-14 14:01:52 -0600

    First Commit

---
+++ testCode1.py
@@ -0,0 +1,16 @@
+
+#from nltk.book import *
+
+#print(sorted(set(text3)))
+
+#numOfWords = text5.count("")
+
+#print(100 * numOfWords / len(text5))
+
+#text5.generate()
+
+#print(100 * (text5.count("lol") / len(text5)))
+
+#def percentageOfWords(count, total):
+    #return 100 * (count / total)
+
